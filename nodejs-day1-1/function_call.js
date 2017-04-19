4e3ae1b04bdeb275d3d79d38e9910d51d50542ae
djtgithub <13552030296@163.com>
2017-04-19 15:32:54 +0800

    function_call.js

---
+++ nodejs-day1-1/function_call.js
@@ -0,0 +1,18 @@
+//console.log('hello world')
+ var http=require('http');
+ http.createServer(function(request,response){ //创建一个服务
+ 			//response 从服务器端往浏览器端输出内容
+ 			//request 接收浏览器端来的信息
+ 			response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+ 			if(request.url !='/favicon.ico'){
+ 				console.log('hello world');
+	 			response.write('hello world');
+	 			hello(response);
+	 			response.end();
+ 			}
+ 			
+ }).listen(3000);
+ console.log('Server running at http://127.0.0.1:3000/');
+ function hello(res){
+ 	res.write('hello nodejs');
+ }
